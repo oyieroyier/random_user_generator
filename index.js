a1335001c9af4e1fa948802125ebd04d6ba563bb
Bob Oyier <boboyier92@gmail.com>
2023-03-27 21:22:13 +0300

    First Vue JS task 😉

---
+++ index.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+	data() {
+		return {
+			firstName: 'Jane',
+			lastName: 'Doe',
+			email: 'janedoe@gmail.com',
+			gender: 'female',
+			picture: 'https://randomuser.me/api/portraits/women/8.jpg',
+		};
+	},
+
+	methods: {
+		async getUser() {
+			const res = await fetch('https://randomuser.me/api/');
+			const { results } = await res.json();
+
+			this.firstName = results[0].name.first;
+			this.lastName = results[0].name.last;
+			this.email = results[0].email;
+			this.gender = results[0].gender;
+			this.picture = results[0].picture.large;
+		},
+	},
+});
+
+app.mount('#app');
